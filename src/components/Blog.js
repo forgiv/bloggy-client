c8de56d5ac4dad883d7f11ae5ee32f21013361dc
Hiram Cruz <hiram0101@gmail.com>
2018-05-29 19:20:03 -0400

    added blog title to blog page

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,21 +1,41 @@
 import React from 'react'
 import connect from 'react-redux/lib/connect/connect'
 import { getPosts } from '../actions/post'
+import { apiURL } from '../config'
 
 class Blog extends React.Component {
-  componentWillMount() {
-    this.props.dispatch(getPosts(this.props.match.params.username))
+  constructor(props) {
+    super(props)
+    this.state = {
+      blog: null,
+      error: null
+    }
+  }
+
+  componentDidMount() {
+    const username = this.props.match.params.username
+    this.props.dispatch(getPosts(username))
+    fetch(`${apiURL}/users/${username}`)
+      .then(res => res.json())
+      .then(data => {
+        data.blog
+          ? this.setState({ blog: data.blog })
+          : this.setState({ error: data.message })
+      })
+      .catch(err => this.setState({ error: err }))
   }
 
   render() {
-    if (this.props.error) return <div>{this.props.error}</div>
+    if (this.props.error || this.state.error)
+      return <div>{this.props.error || this.state.error}</div>
 
     return (
       <div>
-        <h1>{this.props.match.params.username}</h1>
+        <h1>{this.state.blog}</h1>
+        <h3>{this.props.match.params.username}</h3>
         {this.props.posts.map(post => {
           return (
-            <article>
+            <article key={post.title}>
               <header>{post.title}</header>
               {post.content}
               <footer>
