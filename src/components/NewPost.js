df207e6dd92f4b9d2e3b9d754386f3b99de2de45
Hiram Cruz <hiram0101@gmail.com>
2018-05-29 18:07:36 -0400

    updated newpost component

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import './styles/NewPost.css'
+import { connect } from 'react-redux'
+import { apiURL } from '../config'
+import { Redirect } from 'react-router-dom'
 
 class NewPost extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      success: false
+    }
+  }
+
   submitForm = e => {
     e.preventDefault()
+    fetch(`${apiURL}/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title: this.title.value,
+        content: this.content.value
+      }),
+      headers: {
+        'content-type': 'application/json',
+        authorization: `Bearer ${this.props.authToken}`
+      }
+    })
+      .then(
+        res =>
+          (res.status = 201) ? this.setState({ success: true }) : res.json()
+      )
+      .then(data => this.setState({ error: data.message }))
+      .catch(err => this.setState({ error: err }))
   }
 
   render() {
+    if (this.state.success) return <Redirect to="/dashboard" />
     return (
       <form onSubmit={e => this.submitForm(e)}>
         <label htmlFor="title">title</label>
@@ -17,11 +46,16 @@ class NewPost extends React.Component {
           ref={e => (this.title = e)}
         />
         <label htmlFor="content">content</label>
-        <textarea id="content" name="content" />
+        <textarea id="content" name="content" ref={e => (this.content = e)} />
         <input type="submit" value="publish" />
+        <div>{this.state.error}</div>
       </form>
     )
   }
 }
 
-export default NewPost
+const mapStateToProps = state => ({
+  authToken: state.auth.authToken
+})
+
+export default connect(mapStateToProps)(NewPost)
