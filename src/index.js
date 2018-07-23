54b6450a5461383bd851aeb47cc333ce3fed95b8
Hiram Cruz <hiram0101@gmail.com>
2018-07-23 09:28:44 -0400

    reorganized imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import { Provider } from 'react-redux'
+
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
-import { Provider } from 'react-redux'
+import './index.css'
+
+import App from './components/App'
 
 ReactDOM.render(
   <Provider store={store}>
