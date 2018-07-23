3fec111fd498c6073044ed66e5d7213c5331c1b2
Hiram Cruz <hiram0101@gmail.com>
2018-07-23 09:27:13 -0400

    intergrated new local storage methods and reorganized imports

--- src/store.js
+++ src/store.js
@@ -1,14 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+
+import { refreshAuthToken, setAuthToken } from './actions/auth'
+import { loadAuthToken } from './local-storage'
 import { rootReducer } from './reducers'
-import { refreshAuthToken } from './actions/auth'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
-const authToken = localStorage.getItem('authToken')
+const authToken = loadAuthToken()
 if (authToken) {
-  store.dispatch(refreshAuthToken(authToken))
+  const token = authToken
+  store.dispatch(setAuthToken(token))
+  store.dispatch(refreshAuthToken())
 }
 
 export default store
